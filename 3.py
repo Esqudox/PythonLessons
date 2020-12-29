e07be170bfede2d99e0a6e8b6a6d90893b20a6a4
Andrew <jessrabbit93@gmail.com>
2020-12-29 12:59:16 +0300

    1st homework

---
+++ 3.py
@@ -0,0 +1,6 @@
+n = int(input('Введите любую цифру - '))
+
+# переводим n str, для составления числа, после чего переводим обратно в int и складываем
+summa = (n + int(str(n) + str(n)) + int(str(n) + str(n) + str(n)))
+
+print(f'Сумма n + nn + nnn = {summa}')
